7598d1bb8ec9449f991c34e9b34bdf3055c3451b
Linh B Ngo <lngo@wcupa.edu>
2022-09-23 11:20:48 -0400

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -13,4 +13,18 @@ echo "/opt/keys 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" | sudo tee
 sudo systemctl restart nfs-kernel-server
 
 kubeadm init > /opt/keys/kube.log
+
+sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml /local/repository/kube-apiserver.yaml.backup
+sudo sed -i '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=5000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml
+
+sleep 90
+
 sudo touch /opt/keys/kube_done
+
+while IFS= read -r line; do
+  mkdir -p /users/$line/.kube
+  sudo cp -i /etc/kubernetes/admin.conf /users/$line/.kube/config
+  sudo chown $line: /users/$line/.kube/config
+done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
+
+sudo -H -u $1 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
