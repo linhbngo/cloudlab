c799c9d0b018d5014e19e95ba9fb4884e263ad9a
Linh B Ngo <lngo@wcupa.edu>
2022-09-23 11:17:08 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -3,11 +3,33 @@ import geni.rspec.pg as pg
 import geni.rspec.igext as IG
    
 pc = portal.Context()
+
+pc.defineParameter( "n", 
+                   "Number of nodes (3 or more)", 
+                   portal.ParameterType.INTEGER, 3 )
+pc.defineParameter( "userid", 
+                   "CloudLab user ID to deploy K8s from (should be your CloudLab ID. Defaulted to none", 
+                   portal.ParameterType.STRING, 'none' )
+pc.defineParameter( "corecount", 
+                   "Number of cores in each node (4 or more).  NB: Make certain your requested cluster can supply this quantity.", 
+                   portal.ParameterType.INTEGER, 4 )
+pc.defineParameter( "ramsize", "MB of RAM in each node (8192 or more).  NB: Make certain your requested cluster can supply this quantity.", 
+                   portal.ParameterType.INTEGER, 8192 )
+params = pc.bindParameters()
+
 request = pc.makeRequestRSpec()
+
+if params.n < 3:
+  portal.context.reportError( portal.ParameterError( "You must request at least 3 nodes." ) )
+if params.corecount < 4:
+  portal.context.reportError( portal.ParameterError( "You must request at least 4 cores per node." ) )
+if params.ramsize < 8192:
+  portal.context.reportError( portal.ParameterError( "You must request at least 8192 MB of RAM per node." ) )
+
 
 tourDescription = \
 """
-This profile provides the template for 3 compute nodes with Docker and Kubernetes installed on Ubuntu 18.04
+This profile provides the template for Docker and Kubernetes installed on Ubuntu 18.04
 """
 
 #
@@ -20,7 +42,7 @@ request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-num_nodes = 3
+num_nodes = params.n
 for i in range(num_nodes):
   if i == 0:
     node = request.XenVM("head")
@@ -28,8 +50,10 @@ for i in range(num_nodes):
     bs_landing.size = "500GB"
   else:
     node = request.XenVM("worker-" + str(i))
-  node.cores = 4
-  node.ram = 8192
+  node.cores = params.corecount
+  node.ram = params.ramsize
+  bs_landing = node.Blockstore("bs_" + str(i), "/image")
+  bs_landing.size = "500GB"
   node.routable_control_ip = "true" 
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
@@ -45,7 +69,7 @@ for i in range(num_nodes):
   
   if i == 0:
     # install Kubernetes manager
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh " + params.userid))
     # install Helm
     node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_helm.sh"))
   else:
