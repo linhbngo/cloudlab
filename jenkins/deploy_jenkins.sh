9a4f01095fbe1eed8c36d0e0768156273c6a9c25
Linh B Ngo <lngo@wcupa.edu>
2022-09-23 08:55:32 -0400

    Update deploy_jenkins.sh

--- jenkins/deploy_jenkins.sh
+++ jenkins/deploy_jenkins.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -x
+
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
 
-set -x
-
-helm install -f /local/repository/jenkins/values.yaml jenkins/jenkins --generate-name
+export KUBEHEAD=$(kubectl get nodes -o custom-columns=NAME:.status.addresses[1].address,IP:.status.addresses[0].address | grep head | awk -F ' ' '{print $2}')
+cp /local/repository/jenkins/values.yaml .
+sed -i "s/KUBEHEAD/$(cat /opt/keys/headnode)/g" values.yaml
+helm install -f values.yaml jenkins/jenkins --generate-name
