d9b3ded03c99633d2e0c04bc710b1e9e115c89a9
Linh B Ngo <lngo@wcupa.edu>
2022-09-26 20:45:36 -0400

    Update deploy_jenkins.sh

--- jenkins/deploy_jenkins.sh
+++ jenkins/deploy_jenkins.sh
@@ -9,3 +9,7 @@ export KUBEHEAD=$(kubectl get nodes -o custom-columns=NAME:.status.addresses[1].
 cp /local/repository/jenkins/values.yaml .
 sed -i "s/KUBEHEAD/$(cat /opt/keys/headnode)/g" values.yaml
 helm install -f values.yaml jenkins/jenkins --generate-name
+
+kubectl create sa jenkins
+kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts
+kubectl create clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=default:jenkins
