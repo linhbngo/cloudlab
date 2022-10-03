39aded74b0ccd1e5d7c2e1d52e077e1126ade3b0
Linh B Ngo <lngo@wcupa.edu>
2022-10-02 22:55:13 -0400

    Update deploy_registry.sh

--- registry/deploy_registry.sh
+++ registry/deploy_registry.sh
@@ -2,6 +2,6 @@
 
 # adapted and automated basaed on the instructions from https://blog.zachinachshon.com/docker-registry/
 
-kubectl create namespace containter-registry
+kubectl create namespace container-registry
 
 bash /local/repository/cert-manager/deploy_cert_manager.sh
