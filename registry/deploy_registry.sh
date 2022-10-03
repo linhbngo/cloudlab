8de499f0f1cdd7fb7f690120cdcdb85cf2c63de4
Linh B Ngo <lngo@wcupa.edu>
2022-10-03 18:49:27 -0400

    Update deploy_registry.sh

--- registry/deploy_registry.sh
+++ registry/deploy_registry.sh
@@ -2,9 +2,6 @@
 set -x
 # adapted and automated basaed on the instructions from https://blog.zachinachshon.com/docker-registry/
 
-kubectl create namespace container-registry
-
-bash /local/repository/cert-manager/deploy_cert_manager.sh
 bash /local/repository/registry/gen_htpasswd.sh
 helm repo add twuni https://helm.twun.io
 
