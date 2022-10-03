91193884b248f06acb74fe40fcdde0548e8513ba
Linh B Ngo <lngo@wcupa.edu>
2022-10-03 14:47:58 -0400

    Update deploy_registry.sh

--- registry/deploy_registry.sh
+++ registry/deploy_registry.sh
@@ -16,11 +16,11 @@ helm upgrade --install docker-registry \
     --version 1.10.1
    
 cp /local/repository/registry/ingress.yaml .
-sed -i "s/MYDOMAIN/$(hostname -d)/g" ingress.yaml
-sed -i "s/MYHOST/$(hostname -f)/g" ingress.yaml
+#sed -i "s/MYDOMAIN/$(hostname -d)/g" ingress.yaml
+#sed -i "s/MYHOST/$(hostname -f)/g" ingress.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.8/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
 kubectl apply -f ingress.yaml
 
-# Alternatively, add a new hosted name entry with a one-liner
+echo "Alternatively, add a new hosted name entry with a one-liner"
 # echo -e "111.222.333.444\tregistry.MY_DOMAIN.com" | sudo tee -a /etc/hosts
