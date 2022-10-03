07b61bc64237556709922511e42d56f8f6c74d30
Linh B Ngo <lngo@wcupa.edu>
2022-10-03 15:41:30 -0400

    Create deploy_traefik_ingress.sh

---
+++ ingress/deploy_traefik_ingress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kubectl label nodes $(hostname) node-type=master
+kubectl create namespace traefik
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+helm upgrade --install traefik \
+    --namespace traefik \
+    --set dashboard.enabled=true \
+    --set rbac.enabled=true \
+    --set="additionalArguments={--api.dashboard=true,--log.level=INFO,--providers.kubernetesingress.ingressclass=traefik-internal,--serversTransport.insecureSkipVerify=true}" \
+    traefik/traefik \
+    --version 9.1.1
+    
+    
