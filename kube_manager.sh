461691158e44e9c43de2738a91653d537d324843
Linh B Ngo <lngo@wcupa.edu>
2022-10-02 21:46:36 -0400

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -28,4 +28,7 @@ while IFS= read -r line; do
   sudo chown $line: /users/$line/.kube/config
 done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
 
-sudo -H -u $1 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+# weave is outdated
+#sudo -H -u $1 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+sudo -H -u $1 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
