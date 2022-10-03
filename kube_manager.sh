d52b4350b401d6db396131f67826562250ddcff0
Linh B Ngo <lngo@wcupa.edu>
2022-10-02 22:31:24 -0400

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -13,7 +13,8 @@ do
 done
 sudo systemctl restart nfs-kernel-server
 
-kubeadm init > /opt/keys/kube.log
+# this subnet is to support flannel
+kubeadm init --pod-network-cidr=10.244.0.0/16 > /opt/keys/kube.log
 
 sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml /local/repository/kube-apiserver.yaml.backup
 sudo sed -i '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=5000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml
@@ -28,7 +29,7 @@ while IFS= read -r line; do
   sudo chown $line: /users/$line/.kube/config
 done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
 
-# weave is outdated
+# weave is not contactable
 #sudo -H -u $1 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 sudo -H -u $1 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
