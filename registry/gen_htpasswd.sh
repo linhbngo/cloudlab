4b31f149f027832d124cb9bc826455e8f1b29c35
Linh B Ngo <lngo@wcupa.edu>
2022-10-02 22:57:48 -0400

    Create gen_htpasswd.sh

---
+++ registry/gen_htpasswd.sh
@@ -0,0 +1,15 @@
+# Change these credentials to your own
+export REGISTRY_USER=admin
+export REGISTRY_PASS=registry
+export DESTINATION_FOLDER=${HOME}/temp/registry-creds
+   
+# Backup credentials to local files (in case you'll forget them later on)
+mkdir -p ${DESTINATION_FOLDER}
+echo ${REGISTRY_USER} >> ${DESTINATION_FOLDER}/registry-user.txt
+echo ${REGISTRY_PASS} >> ${DESTINATION_FOLDER}/registry-pass.txt
+   	
+docker run --entrypoint htpasswd registry:2.7.0 \
+    -Bbn ${REGISTRY_USER} ${REGISTRY_PASS} \
+    > ${DESTINATION_FOLDER}/htpasswd
+      
+unset REGISTRY_USER REGISTRY_PASS DESTINATION_FOLDER
