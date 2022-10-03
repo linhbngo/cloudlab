7ffeb5e0abe380112bd4a666020376d13714f0ae
Linh B Ngo <lngo@wcupa.edu>
2022-10-03 14:10:26 -0400

    Update deploy_cert_manager.sh

--- cert-manager/deploy_cert_manager.sh
+++ cert-manager/deploy_cert_manager.sh
@@ -8,13 +8,13 @@ helm upgrade --install cert-manager --namespace cert-manager jetstack/cert-manag
 
 
 cp /local/repository/cert-manager/cluster-issuer.yaml .
-sed -i "s/MYDOMAIN/$(hostname -d)/g" cluster-issuer.yaml
+#sed -i "s/MYDOMAIN/$(hostname -d)/g" cluster-issuer.yaml
 kubectl apply -f cluster-issuer.yaml
 
 kubectl get clusterissuers $(hostname -d)-ca-issuer -o wide
 
 cp /local/repository/cert-manager/x509.yaml .
-sed -i "s/MYDOMAIN/$(hostname -d)/g" x509.yaml
+#sed -i "s/MYDOMAIN/$(hostname -d)/g" x509.yaml
 kubectl apply -f x509.yaml
 
 kubectl describe certificate $(hostname -d)-com-cert --namespace container-registry
