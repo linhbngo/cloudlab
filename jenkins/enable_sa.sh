cd2bda155f5c535db584a7d470bb2d784506380b
Linh B Ngo <lngo@wcupa.edu>
2022-10-11 13:11:00 -0400

    Create enable_sa.sh

---
+++ jenkins/enable_sa.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl create sa jenkins
+kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts
+kubectl create clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=default:jenkins
