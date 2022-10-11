f39e2fef1b8b258f92ac3e6845c2214b25ce5ce6
Linh B Ngo <lngo@wcupa.edu>
2022-10-11 16:24:18 -0400

    Update deploy_registry.sh

--- registry/deploy_registry.sh
+++ registry/deploy_registry.sh
@@ -6,16 +6,15 @@ bash /local/repository/registry/gen_htpasswd.sh
 helm repo add twuni https://helm.twun.io
 
 helm upgrade --install docker-registry \
-    --namespace container-registry \
     --set replicaCount=1 \
     --set secrets.htpasswd=$(cat $HOME/temp/registry-creds/htpasswd) \
     twuni/docker-registry \
     --version 1.10.1
    
-cp /local/repository/registry/ingress.yaml .
-sed -i "s/MYDOMAIN/$(hostname -d)/g" ingress.yaml
+#cp /local/repository/registry/ingress.yaml .
+#sed -i "s/MYDOMAIN/$(hostname -d)/g" ingress.yaml
 
-kubectl apply -f ingress.yaml
+#kubectl apply -f ingress.yaml
 
-echo "Alternatively, add a new hosted name entry with a one-liner"
+#echo "Alternatively, add a new hosted name entry with a one-liner"
 # echo -e "111.222.333.444\tregistry.MY_DOMAIN.com" | sudo tee -a /etc/hosts
