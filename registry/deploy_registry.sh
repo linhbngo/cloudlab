ee19c4803a697eebf0587c3397804ba50b98f233
Linh B Ngo <lngo@wcupa.edu>
2022-11-15 09:58:29 -0500

    Update deploy_registry.sh

--- registry/deploy_registry.sh
+++ registry/deploy_registry.sh
@@ -10,11 +10,4 @@ helm upgrade --install docker-registry \
     --set secrets.htpasswd=$(cat $HOME/temp/registry-creds/htpasswd) \
     twuni/docker-registry \
     --version 1.10.1
-   
-#cp /local/repository/registry/ingress.yaml .
-#sed -i "s/MYDOMAIN/$(hostname -d)/g" ingress.yaml
-
-#kubectl apply -f ingress.yaml
-
-#echo "Alternatively, add a new hosted name entry with a one-liner"
-# echo -e "111.222.333.444\tregistry.MY_DOMAIN.com" | sudo tee -a /etc/hosts
+ 
