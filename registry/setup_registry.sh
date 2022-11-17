92d3bbe4b280bdf1424b1fe53f02818d5b57a686
Linh B Ngo <lngo@wcupa.edu>
2022-11-16 20:23:31 -0500

    Create setup_registry.sh

---
+++ registry/setup_registry.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+1  clear
+    2  mkdir registry
+    3  cd registry/
+    4  mkdir certs
+    5  mkdir auth
+    6  cd certs/
+    7  openss genrsa 1024 > domain.key
+    8  openssl genrsa 1024 > domain.key
+    9  chmod 400 domain.key
+   10  nano san.cnf
+   11  ip addr | grep 130
+   12  ip addr
+   13  ping clnodevm200-1.clemson.cloudlab.us
+   14  nano san.cnf
+   15  openssl req -new -x509 -nodes -sha1 -days 365 -key domain.key -out domain.crt -config san.cnf
+   16  cd ../auth
+   17  docker run --rm --entrypoint htpasswd registry:2.7.0 -Bbn admin registry > htpasswd
+   18  cd ..
