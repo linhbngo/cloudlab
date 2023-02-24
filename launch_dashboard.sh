11458b842f235dbed8380770dfdba2d5c4900886
Linh B Ngo <lngo@wcupa.edu>
2023-02-24 10:34:59 -0500

    Update launch_dashboard.sh

--- launch_dashboard.sh
+++ launch_dashboard.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
-
+kubectl apply -f k8s_dashboard/dashboard-insecure.yaml
+kubectl apply -f k8s_dashboard/socat.yaml
+kubectl get svc --namespace=kubernetes-dashboard
+kubectl patch service kubernetes-dashboard -n kubernetes-dashboard --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30082}]'
