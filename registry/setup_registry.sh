4d5ef95842790865bf92370fbda2a80b8e00aa96
Linh B Ngo <lngo@wcupa.edu>
2023-04-17 08:42:45 -0400

    Update setup_registry.sh

--- registry/setup_registry.sh
+++ registry/setup_registry.sh
@@ -6,7 +6,12 @@ cd /opt/keys/certs
 openssl genrsa 1024 > domain.key
 chmod 400 domain.key
 cp /local/repository/registry/san.cnf.template san.cnf
-ip_address=$(nslookup $(hostname -f) | grep Server | awk -F ' ' '{printf $2}')
+
+# In some cases we might have to switch to this to get ip address. 
+# ip_address=$(nslookup $(hostname -f) | grep Server | awk -F ' ' '{printf $2}')
+
+ip_address=$(ip addr | grep eth0| awk -F ' ' '{print $2}' | awk -F '/' '{print $1'})
+
 sed -i "s/IPADDR/${ip_address}/g" san.cnf
 openssl req -new -x509 -nodes -sha1 -days 365 -key domain.key -out domain.crt -config san.cnf
 
@@ -18,10 +23,3 @@ docker run --rm --entrypoint htpasswd registry:2.7.0 -Bbn admin registry > htpas
 # create a template subdirectory to be mounted to pods
 mkdir -p /opt/keys/certs.d/${ip_address}:443
 cp /opt/keys/certs/domain.crt /opt/keys/certs.d/${ip_address}:443/ca.crt
-
-# on Kubernetes pod
-#sudo mkdir -p /etc/docker/certs.d/130.127.132.216:443
-#    2  sudo nano /etc/docker/certs.d/130.127.132.216\:443/
-#    3  sudo nano /etc/docker/certs.d/130.127.132.216\:443/ca.crt
-#    4  docker login -u admin -p https://130.127.132.216:443
-#    5  docker login -u admin -p registry https://130.127.132.216:443
