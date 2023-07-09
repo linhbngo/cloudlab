3b728da77232cc1affe6b15fac38e989343c7078
Linh B Ngo <lngo@wcupa.edu>
2023-07-09 08:57:18 -0400

    Update install_puppet.sh

--- install_puppet.sh
+++ install_puppet.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # setup Java
+dnf update -y
 dnf install -y java-11-openjdk.x86_64
 
 # get Puppet source
+dnf config-manager --set-enable crb
+ 
 rpm -Uvh http://yum.puppet.com/puppet-release-el-9.noarch.rpm
+
+yum install puppetserver
 
 
