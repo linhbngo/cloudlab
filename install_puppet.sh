2ec0e3993d5441b177b51324e34fa927580b71d3
Linh B Ngo <lngo@wcupa.edu>
2023-07-09 07:23:06 -0400

    Update install_puppet.sh

--- install_puppet.sh
+++ install_puppet.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# setup Java
+wget https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.19%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.19_7.tar.gz
+
+# get Puppet source
 sudo rpm -Uvh http://yum.puppet.com/puppet-release-el-9.noarch.rpm
+
+
