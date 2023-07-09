7505ada7d8b71e923c4273903482f23065245f78
Linh B Ngo <lngo@wcupa.edu>
2023-07-09 10:22:19 -0400

    Update install_puppet.sh

--- install_puppet.sh
+++ install_puppet.sh
@@ -5,9 +5,14 @@ dnf install -y java-11-openjdk.x86_64
 
 # get Puppet source
 dnf config-manager --set-enable powertools
- 
 rpm -Uvh http://yum.puppet.com/puppet-release-el-9.noarch.rpm
 dnf update -y
-yum install -y puppetserver
 
+# install puppet server
+if [ $1 = "server" ]; then
+  dnf install -y puppetserver
+  sudo sed -i 's/-Xms2g -Xmx2g/-Xms512m -Xmx512m/g' /etc/sysconfig/puppetserver
+else
+
+fi
 
