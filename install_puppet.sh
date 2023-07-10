1c9748ca92854bddf968b2bd5ad8ead2cd04254e
Linh B Ngo <lngo@wcupa.edu>
2023-07-09 20:11:54 -0400

    Update install_puppet.sh

--- install_puppet.sh
+++ install_puppet.sh
@@ -5,7 +5,7 @@ dnf install -y java-11-openjdk.x86_64
 
 # get Puppet source
 dnf config-manager --set-enable powertools
-rpm -Uvh http://yum.puppet.com/puppet-release-el-9.noarch.rpm
+rpm -Uvh http://yum.puppet.com/puppet-release-el-7.noarch.rpm
 dnf update -y
 
 # install puppet server
