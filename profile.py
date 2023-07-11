95ebd695b8b8dea254b614ca16bf45362893928c
Linh B Ngo <lngo@wcupa.edu>
2023-07-10 23:59:57 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -36,41 +36,49 @@ link = request.LAN("lan")
 num_nodes = params.nk8s + params.nhpc + 2
 k8s_count = 1;
 compute_count = 1;
+
+def setupNode(nodeName, nodeCore, nodeMem, bs, public, nodeID):
+   node = request.XenVM(nodeName)
+   node.cores = nodeCore
+   node.ram = nodeMem
+   if bs:
+      bs_landing = node.Blockstore("bs_" + nodeName, "/image")
+      bs_landing.size = "500GB"
+   if public:
+      node.routable_control_ip = "true" 
+   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+   return node
+
 for i in range(num_nodes):
-  nodename = ""
-  if i == 0:
-    nodename = "cas"
-  elif i == 1:
-    nodename = "master"
-  elif i >= 2 and i <= params.nk8s:
-    nodename = "k8s-" + str(k8s_count)
-    k8s_count += 1
-  else:
-    nodename = "compute-" + str(compute_count)
-    compute_count += 1
-     
-  node = request.XenVM(nodename)
-  node.cores = params.corecount
-  node.ram = params.ramsize
-  bs_landing = node.Blockstore("bs_" + nodename, "/image")
-  bs_landing.size = "500GB"
-  node.routable_control_ip = "true" 
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  
+   nodename = ""
+   if i == 0:
+      nodename = "cas"
+      node = setupNode(nodename, 1, 1024, False, False, i)
+   elif i == 1:
+      nodename = "master"
+      node = setupNode(nodename, params.corecount, params.ramsize, True, True, i)
+   elif i >= 2 and i <= params.nk8s:
+      nodename = "k8s-" + str(k8s_count)
+      node = setupNode(nodename, params.corecount, params.ramsize, True, True, k8s_count)
+      k8s_count += 1
+   else:
+      nodename = "compute-" + str(compute_count)
+      node = setupNode(nodename, params.corecount, params.ramsize, True, True, compute_count)
+      compute_count += 1
+   iface = node.addInterface("if" + str(i))
+   iface.component_id = "eth1"
+   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+   link.addInterface(iface)
+   
   # install Docker
   #node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   # install Kubernetes
   #node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
   #node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
-  
-  if i == 0:
-    # install Puppet
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_puppet_ubuntu.sh server " + str(num_nodes)))
-  else:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_puppet_ubuntu.sh " + nodename))
+   if i == 0:
+      # install Puppet
+      node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_puppet_ubuntu.sh server " + str(num_nodes)))
+   else:
+      node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_puppet_ubuntu.sh " + nodename))
 
 pc.printRequestRSpec(request)
