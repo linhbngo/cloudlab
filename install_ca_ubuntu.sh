edf54ae4b6232f5fbe301b042c5f41cd80b0803a
Linh B Ngo <lngo@wcupa.edu>
2023-07-11 03:01:04 -0400

    Create install_ca_ubuntu.sh

---
+++ install_ca_ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-and-configure-a-certificate-authority-ca-on-ubuntu-20-04
+# https://www.shellhacks.com/create-csr-openssl-without-prompt-non-interactive/
