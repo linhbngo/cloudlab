d11c471887e156b16b4f995072643c832b05b236
Linh B Ngo <lngo@wcupa.edu>
2024-03-03 22:23:04 -0500

    Update install_kubernetes.sh

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get update && sudo apt-get install -y apt-transport-https curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
-
-curl -L https://github.com/kubernetes/kompose/releases/download/v1.28.0/kompose-linux-amd64 -o kompose
-chmod +x kompose
-mv ./kompose /usr/local/bin/kompose
