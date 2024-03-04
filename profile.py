01ac7a2dcf8e3e53dc69245a433799033d9c417b
Linh B Ngo <lngo@wcupa.edu>
2024-03-04 09:48:55 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -56,7 +56,7 @@ for i in range(num_nodes):
   
   if i == 0:
     # install Kubernetes manager
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh " + params.userid + " " + str(num_nodes)))
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh " + str(num_nodes)))
     # install Helm
     node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_helm.sh"))
   else:
