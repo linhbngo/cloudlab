cbca446f25de8842e2a45ea0f9f5fcc0e4c26506
Linh B Ngo <lbn28@drexel.edu>
2024-03-04 21:05:01 -0500

    remove redundancies

--- setup_dns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "DNS=10.96.0.10" | sudo tee /etc/systemd/resolved.conf
-sudo systemctl daemon-reload
-sudo systemctl restart systemd-networkd
-sudo systemctl restart systemd-resolved
