9e465f07b3789f185cb0ac10e83ad7e0be724465
Linh B Ngo <lngo@wcupa.edu>
2024-04-12 15:27:29 -0400

    Create setup_insecure.sh

---
+++ registry/setup_insecure.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl create deployment registry --image=registry
+kubectl expose deploy/registry --port=5000 --type=NodePort
+kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":5000}]'
