17922a620958ecf8a2a948e50a1bb242715d5cbb
Linh B Ngo <lngo@wcupa.edu>
2024-04-15 14:53:29 -0400

    Update setup_insecure.sh

--- registry/setup_insecure.sh
+++ registry/setup_insecure.sh
@@ -2,4 +2,4 @@
 
 kubectl create deployment registry --image=registry
 kubectl expose deploy/registry --port=5000 --type=NodePort
-kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":5000}]'
+kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30500}]'
