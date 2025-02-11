859c58d587afe8d8f0e79e34324684d993e59817
Linh B Ngo <lngo@wcupa.edu>
2025-02-10 19:22:08 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo apt-get install -y httping
 sudo apt-get install -y jq
 
-curl -L https://github.com/docker/compose/releases/download/1.28.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/v2.32.4/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # the username needs to be changed
