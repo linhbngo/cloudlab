7db2e8fbbffcbdbcf552b92ee4bc242fbb972621
Linh B Ngo <lngo@wcupa.edu>
2026-01-19 15:40:43 -0500

    Update profile for Ubuntu version change to 22.04

--- profile.py
+++ profile.py
@@ -7,7 +7,7 @@ request = pc.makeRequestRSpec()
 
 tourDescription = \
 """
-This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
+This profile provides the template for a compute node with Docker installed on Ubuntu 22.04
 """
 
 #
@@ -23,7 +23,7 @@ node.routable_control_ip = "true"
 bs_landing = node.Blockstore("bs_image", "/image")
 bs_landing.size = "500GB"
   
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   
